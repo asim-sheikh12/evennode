798ff9e008a768480633eacdbd3f4ecefac41603
Asim <asim.thoughtwin@gmail.com>
2022-12-08 15:53:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+app.use(cors("*"));
+const port = 3000;
+
+app.get("/user", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "http://jsonplaceholder.typicode.com/users"
+    );
+    return res.status(200).json({ data: response.data });
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
